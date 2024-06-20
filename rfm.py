f9fd1b01bea820de84dd96c7d627b12fd5a9cb17
husickova <156668585+husickova@users.noreply.github.com>
2024-06-20 15:27:52 +0200

    Update rfm.py

--- rfm.py
+++ rfm.py
@@ -113,17 +113,6 @@ try:
         st.plotly_chart(fig)
         st.markdown("<p style='font-size: small;'>Monetary shows how much money each customer spends.</p>", unsafe_allow_html=True)
 
-    # Filter data based on the selected category
-    if selected_category != 'All':
-        filtered_category_df = rfm_df[rfm_df['Category'] == selected_category]
-        # Display the main graph for the selected category using Plotly
-        fig = px.bar(filtered_category_df, x='id', y='Monetary', title=f'Monetary value for {selected_category}', labels={'id': 'Customer ID', 'Monetary': 'Monetary Value'}, color_discrete_sequence=['dodgerblue'])
-    else:
-        # Display the main graph for all categories using Plotly
-        fig = px.bar(rfm_df, x='Category', y='Monetary', title='Monetary value for all categories', labels={'Category': 'Category', 'Monetary': 'Monetary Value'}, color_discrete_sequence=['dodgerblue'])
-        
-    st.plotly_chart(fig)
-
 except FileNotFoundError:
     st.error(f"File not found at path {csv_path}.")
 except Exception as e:
