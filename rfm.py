7d35b227aada8a81974d5d64be3ca81c1f6aa427
husickova <156668585+husickova@users.noreply.github.com>
2024-06-20 14:10:13 +0200

    Update rfm.py

--- rfm.py
+++ rfm.py
@@ -32,6 +32,3 @@ except FileNotFoundError:
     st.error(f"Soubor na cestě {csv_path} nebyl nalezen.")
 except Exception as e:
     st.error(f"Došlo k chybě při načítání souboru: {e}")
-
-if __name__ == "__main__":
-    st.run()
