e39836df6666fd7d0413b4f2e6ca4a59e0298821
husickova <156668585+husickova@users.noreply.github.com>
2024-06-20 13:57:09 +0200

    Update rfm.py

--- rfm.py
+++ rfm.py
@@ -8,8 +8,24 @@ st.title("RFM for Keboola by Bytegarden")
 csv_path = "rfm-data.csv"  # relativní cesta, pokud je skript ve stejné složce
 try:
     df = pd.read_csv(csv_path)
-    # Zobrazení tabulky
-    st.dataframe(df)
+    
+    # Kontrola, zda DataFrame obsahuje sloupec s datem
+    if 'date' not in df.columns:
+        st.error("DataFrame neobsahuje sloupec 'date'.")
+    else:
+        # Převod sloupce 'date' na typ datetime
+        df['date'] = pd.to_datetime(df['date'])
+        
+        # Vytvoření interaktivních polí pro výběr datumu
+        start_date = st.date_input('Start date', df['date'].min().date())
+        end_date = st.date_input('End date', df['date'].max().date())
+        
+        # Filtrování dat podle vybraných dat
+        filtered_df = df[(df['date'] >= pd.to_datetime(start_date)) & (df['date'] <= pd.to_datetime(end_date))]
+        
+        # Zobrazení filtrované tabulky
+        st.dataframe(filtered_df)
+        
 except FileNotFoundError:
     st.error(f"Soubor na cestě {csv_path} nebyl nalezen.")
 except Exception as e:
