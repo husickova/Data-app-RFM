3dd073fa41db723b806b1dd7c3698a124460b228
husickova <156668585+husickova@users.noreply.github.com>
2024-06-20 14:12:36 +0200

    Update rfm.py

--- rfm.py
+++ rfm.py
@@ -1,10 +1,32 @@
 import streamlit as st
 import pandas as pd
+import re
 
 # Titulek aplikace s barevným textem
 st.markdown("""
 # RFM for <span style="color:dodgerblue">Keboola</span> by <span style="color:purple">Bytegarden</span>
 """, unsafe_allow_html=True)
+
+# Funkce pro přiřazení kategorií na základě num_of_events
+def assign_category(num_of_events):
+    patterns = {
+        r'5[4-5]': '01. Champions',
+        r'[3-4][4-5]': '02. Loyal Customers',
+        r'[4-5][2-3]': '03. Potential Loyalists',
+        r'51': '04. Recent Customers',
+        r'41': '05. Promising',
+        r'33': '06. Need Attention',
+        r'3[1-2]': '07. About to Sleep',
+        r'[1-2][5]': '08. Can\'t Lose',
+        r'[1-2][3-4]': '09. At Risk',
+        r'2[1-2]': '10. Hibernating',
+        r'1[1-2]': '11. Lost',
+    }
+    
+    for pattern, category in patterns.items():
+        if re.match(pattern, str(num_of_events)):
+            return category
+    return 'Uncategorized'
 
 # Načtení CSV souboru
 csv_path = "rfm-data.csv"  # relativní cesta, pokud je skript ve stejné složce
@@ -25,7 +47,13 @@ try:
         # Filtrování dat podle vybraných dat
         filtered_df = df[(df['date'] >= pd.to_datetime(start_date)) & (df['date'] <= pd.to_datetime(end_date))]
         
-        # Zobrazení filtrované tabulky
+        # Přiřazení kategorií
+        if 'num_of_events' in filtered_df.columns:
+            filtered_df['Category'] = filtered_df['num_of_events'].apply(assign_category)
+        else:
+            st.error("DataFrame neobsahuje sloupec 'num_of_events'.")
+        
+        # Zobrazení filtrované a kategorizované tabulky
         st.dataframe(filtered_df)
         
 except FileNotFoundError:
