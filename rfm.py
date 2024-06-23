da5a3d60220fdc6246d146f01fe1e01ca43bfea4
husickova <156668585+husickova@users.noreply.github.com>
2024-06-23 20:22:42 +0200

    Update rfm.py

--- rfm.py
+++ rfm.py
@@ -100,19 +100,19 @@ try:
 
     # Create columns for buttons
     col1, col2, col3, col4, col5 = st.columns(5)
-    col6, col7, col8, col9, col10, col11 = st.columns(6)
+    col6, col7, col8, col9, col10 = st.columns(5)
 
     selected_button = None
 
     with col1:
-        if st.button('Histogram Frequency'):
-            selected_button = 'Histogram Frequency'
+        if st.button('Recency'):
+            selected_button = 'Recency'
     with col2:
-        if st.button('Histogram Monetary'):
-            selected_button = 'Histogram Monetary'
+        if st.button('Frequency'):
+            selected_button = 'Frequency'
     with col3:
-        if st.button('Heatmap RFM Segments'):
-            selected_button = 'Heatmap RFM Segments'
+        if st.button('Monetary'):
+            selected_button = 'Monetary'
     with col4:
         if st.button('Scatter Recency vs Frequency'):
             selected_button = 'Scatter Recency vs Frequency'
@@ -123,38 +123,37 @@ try:
         if st.button('Scatter Recency vs Monetary'):
             selected_button = 'Scatter Recency vs Monetary'
     with col7:
-        if st.button('Boxplot Recency'):
-            selected_button = 'Boxplot Recency'
-    with col8:
-        if st.button('Boxplot Frequency'):
-            selected_button = 'Boxplot Frequency'
-    with col9:
-        if st.button('Boxplot Monetary'):
-            selected_button = 'Boxplot Monetary'
-    with col10:
         if st.button('Pareto Chart'):
             selected_button = 'Pareto Chart'
-    with col11:
+    with col8:
         if st.button('About categories'):
             category_counts = rfm_df['Category'].value_counts().reindex(category_order, fill_value=0).reset_index()
             category_counts.columns = ['Category', 'Count']
+            fig = px.bar(category_counts, x='Category', y='Count', title='Category Distribution', color_discrete_sequence=['dodgerblue'])
+            st.plotly_chart(fig)
             st.write(category_counts)
 
     # Display the graph based on the selected button
-    if selected_button == 'Histogram Frequency':
-        fig = px.histogram(filtered_category_df, x='Frequency', title='Histogram Frequency', color_discrete_sequence=['dodgerblue'])
-        st.plotly_chart(fig)
+    if selected_button == 'Recency':
+        fig1 = px.histogram(filtered_category_df, x='Recency', title='Histogram Recency', color_discrete_sequence=['dodgerblue'])
+        fig2 = px.box(filtered_category_df, y='Recency', title='Boxplot Recency', color_discrete_sequence=['dodgerblue'])
+        st.plotly_chart(fig1)
+        st.plotly_chart(fig2)
+        st.markdown("<p style='font-size: small;'>Recency shows how recently each customer made a purchase.</p>", unsafe_allow_html=True)
+
+    if selected_button == 'Frequency':
+        fig1 = px.histogram(filtered_category_df, x='Frequency', title='Histogram Frequency', color_discrete_sequence=['dodgerblue'])
+        fig2 = px.box(filtered_category_df, y='Frequency', title='Boxplot Frequency', color_discrete_sequence=['dodgerblue'])
+        st.plotly_chart(fig1)
+        st.plotly_chart(fig2)
         st.markdown("<p style='font-size: small;'>Frequency shows how often each customer makes a purchase.</p>", unsafe_allow_html=True)
 
-    if selected_button == 'Histogram Monetary':
-        fig = px.histogram(filtered_category_df, x='Monetary', title='Histogram Monetary', color_discrete_sequence=['dodgerblue'])
-        st.plotly_chart(fig)
+    if selected_button == 'Monetary':
+        fig1 = px.histogram(filtered_category_df, x='Monetary', title='Histogram Monetary', color_discrete_sequence=['dodgerblue'])
+        fig2 = px.box(filtered_category_df, y='Monetary', title='Boxplot Monetary', color_discrete_sequence=['dodgerblue'])
+        st.plotly_chart(fig1)
+        st.plotly_chart(fig2)
         st.markdown("<p style='font-size: small;'>Monetary shows how much money each customer spends.</p>", unsafe_allow_html=True)
-
-    if selected_button == 'Heatmap RFM Segments':
-        heatmap_data = filtered_category_df.pivot_table(index='R_rank', columns='F_rank', values='RFM_Score', aggfunc='count').fillna(0)
-        fig = px.imshow(heatmap_data, title='Heatmap RFM Segments', color_continuous_scale='Blues')
-        st.plotly_chart(fig)
 
     if selected_button == 'Scatter Recency vs Frequency':
         fig = px.scatter(filtered_category_df, x='Recency', y='Frequency', title='Scatter Recency vs Frequency', color_discrete_sequence=['dodgerblue'])
@@ -168,30 +167,17 @@ try:
         fig = px.scatter(filtered_category_df, x='Recency', y='Monetary', title='Scatter Recency vs Monetary', color_discrete_sequence=['dodgerblue'])
         st.plotly_chart(fig)
 
-    if selected_button == 'Boxplot Recency':
-        fig = px.box(filtered_category_df, y='Recency', title='Boxplot Recency', color_discrete_sequence=['dodgerblue'])
-        st.plotly_chart(fig)
-
-    if selected_button == 'Boxplot Frequency':
-        fig = px.box(filtered_category_df, y='Frequency', title='Boxplot Frequency', color_discrete_sequence=['dodgerblue'])
-        st.plotly_chart(fig)
-
-    if selected_button == 'Boxplot Monetary':
-        fig = px.box(filtered_category_df, y='Monetary', title='Boxplot Monetary', color_discrete_sequence=['dodgerblue'])
-        st.plotly_chart(fig)
-
     if selected_button == 'Pareto Chart':
-            filtered_category_df_sorted = filtered_category_df.sort_values('Monetary', ascending=False)
-            filtered_category_df_sorted['Cumulative Sum'] = filtered_category_df_sorted['Monetary'].cumsum()
-            filtered_category_df_sorted['Cumulative Percentage'] = 100 * filtered_category_df_sorted['Cumulative Sum'] / filtered_category_df_sorted['Monetary'].sum()
+        filtered_category_df_sorted = filtered_category_df.sort_values('Monetary', ascending=False)
+        filtered_category_df_sorted['Cumulative Sum'] = filtered_category_df_sorted['Monetary'].cumsum()
+        filtered_category_df_sorted['Cumulative Percentage'] = 100 * filtered_category_df_sorted['Cumulative Sum'] / filtered_category_df_sorted['Monetary'].sum()
         
-            fig = px.bar(filtered_category_df_sorted, x='id', y='Monetary', title='Pareto Chart', color_discrete_sequence=['dodgerblue'])
-            fig.add_scatter(x=filtered_category_df_sorted['id'], y=filtered_category_df_sorted['Cumulative Percentage'], mode='lines+markers', name='Cumulative Percentage', marker=dict(color='red'))
-            st.plotly_chart(fig)
-            st.markdown("<p style='font-size: small;'>Pareto chart shows the cumulative contribution of each customer to the total revenue.</p>", unsafe_allow_html=True)
+        fig = px.bar(filtered_category_df_sorted, x='id', y='Monetary', title='Pareto Chart', color_discrete_sequence=['dodgerblue'])
+        fig.add_scatter(x=filtered_category_df_sorted['id'], y=filtered_category_df_sorted['Cumulative Percentage'], mode='lines+markers', name='Cumulative Percentage', marker=dict(color='red', size=8, symbol='circle'))
+        st.plotly_chart(fig)
+        st.markdown("<p style='font-size: small;'>Pareto chart shows the cumulative contribution of each customer to the total revenue.</p>", unsafe_allow_html=True)
 
 except FileNotFoundError:
     st.error(f"File not found at path {csv_path}.")
 except Exception as e:
     st.error(f"An error occurred while loading the file: {e}")
-
