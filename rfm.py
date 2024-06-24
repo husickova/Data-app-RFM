5571a8c78070a1586d92e65649ab4f346bf06f6c
husickova <156668585+husickova@users.noreply.github.com>
2024-06-24 15:23:21 +0200

    Update rfm.py

--- rfm.py
+++ rfm.py
@@ -93,7 +93,7 @@ try:
     rfm_df['R_rank'] = rfm_df['Recency'].apply(lambda x: 5 if x <= recency_params[0] else 4 if x <= recency_params[1] else 3 if x <= recency_params[2] else 2 if x <= recency_params[3] else 1)
     
     # Assign F score
-    rfm_df['F_rank'] = rfm_df['Frequency'].apply(lambda x: 5 if x >= frequency_params[0] else 4 if x >= frequency_params[1] else 3 if x >= frequency_params[2] else 2 if x >= frequency_params[3] else 1)
+    rfm_df['F_rank'] = rfm_df['Frequency'].apply(lambda x: 5 if x <= frequency_params[0] else 4 if x <= frequency_params[1] else 3 if x <= frequency_params[2] else 2 if x <= frequency_params[3] else 1)
     
     # Assign M score based on AOS
     rfm_df['M_rank'] = rfm_df['AOS'].apply(lambda x: 5 if x >= monetary_params[0] else 4 if x >= monetary_params[1] else 3 if x >= monetary_params[2] else 2 if x >= monetary_params[3] else 1)
