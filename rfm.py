774aa8000d4aa8793d98c6833682175099573ca5
husickova <156668585+husickova@users.noreply.github.com>
2024-06-24 11:13:36 +0200

    Update rfm.py

--- rfm.py
+++ rfm.py
@@ -260,7 +260,7 @@ try:
     custom_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', 
                      '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf', '#aec7e8']
 
-     if selected_button == 'About categories':
+    if selected_button == 'About categories':
         # Customizing the display for "About categories"
         fig = px.treemap(
             rfm_df, 
