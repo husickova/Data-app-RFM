fac8727cf96416f10607b158dbdf79aed4699e03
husickova <156668585+husickova@users.noreply.github.com>
2024-06-25 11:44:40 +0200

    Update rfm.py

--- rfm.py
+++ rfm.py
@@ -84,8 +84,16 @@ try:
     rfm_df['R_rank'] = rfm_df['Recency'].apply(lambda x: 5 if x <= 3 else 4 if x <= 10 else 3 if x <= 25 else 2 if x <= 66 else 1)
     
     # Assign F score based on the new methodology
-    rfm_df['F_rank'] = rfm_df['Frequency'].apply(lambda x: 5 if pd.notna(x) and x <= 13.6 else 4 if pd.notna(x) and x <= 24.5 else 3 if pd.notna(x) and x <= 38.8 else 2 if pd.notna(x) and x <= 66.6 else 1)
-    
+    rfm_df['F_rank'] = rfm_df['Frequency'].apply(
+        lambda x: 5 if pd.notna(x) and x <= 13.6 else (
+            4 if pd.notna(x) and x <= 24.5 else (
+                3 if pd.notna(x) and x <= 38.8 else (
+                    2 if pd.notna(x) and x <= 66.6 else 1
+                )
+            )
+        )
+    )
+
     # Assign M score based on AOS
     rfm_df['M_rank'] = rfm_df['AOS'].apply(lambda x: 5 if x >= 6841 else 4 if x >= 3079 else 3 if x >= 1573 else 2 if x >= 672 else 1)
 
